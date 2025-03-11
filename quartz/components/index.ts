84795037fcd8b980aa2196609ad5419f7719abac
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-11 14:56:43 -0700

    feat: flex component, document higher-order layout components

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -20,6 +20,7 @@ import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
+import Flex from "./Flex"
 
 export {
   ArticleTitle,
@@ -46,4 +47,5 @@ export {
   NotFound,
   Breadcrumbs,
   Comments,
+  Flex,
 }
